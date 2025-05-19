686e9b06acc5a8402764d101886a182638a8a68b
heavykaushik1520 <kaushikmandavkar6@gmail.com>
2025-05-19 18:32:55 +0530

    changes

--- jobs/birthdayReminder.js
+++ jobs/birthdayReminder.js
@@ -18,7 +18,7 @@ const getBirthdaysIn2Days = async () => {
 };
 
 const birthdayReminderJob = cron.schedule(
-  "30 18 * * *",
+  "0 9 * * *",
   async () => {
     console.log("Checking for upcoming birthdays...");
     const upcomingBirthdays = await getBirthdaysIn2Days();
@@ -55,7 +55,7 @@ const birthdayReminderJob = cron.schedule(
         </table>
     `;
       await sendMail(
-        "kaushikmandavkar6@gmail.com",
+        "rajneesh.j77@gmail.com",
         `🎂 Upcoming Birthday: ${employee.employeeName}`,
         message
       );
